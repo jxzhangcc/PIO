c6bc8c46845db71f4b17ff1571d449e4322ffdf5
jxzhangcc <38287189+jxzhangcc@users.noreply.github.com>
2020-01-16 16:36:56 +0800

    Add files via upload

--- PIO/PIO.py
+++ PIO/PIO.py
@@ -1,19 +1,20 @@
-import os, sys
+import os
+import sys
 import re
 import numpy as np
-from math import sqrt
-from numpy.linalg import svd, eigh
+import math
+from itertools import repeat
 from paraparser.indexparser import parse_index, rev_parse_index
 from fileparser.parse49 import parse_49
 from filegenerator.genfchk import quicksave
 
-VERSION = 'V3.0'
-VERSIONTEXT = 'Original published version honorably delivered by Acid&Francium (with a blast).\nRevised on 21 May 2019 by Acid.\n'
-
-tol = 1e-6
+VERSION = 'V4.0'
+VERSIONTEXT = 'Original published version honorably delivered by Acid&Francium (with a blast).\nRevised on 16 Jan 2020 by Acid.\n'
+REFERENCE_LIST = '''
+'''
 
 def myeigh(mat, rev=False):
-    evals, evecs = eigh(mat)
+    evals, evecs = np.linalg.eigh(mat)
     evecs = evecs.T
     if rev:
         order = evals.argsort()[::-1]
@@ -23,216 +24,278 @@ def myeigh(mat, rev=False):
     evecs = evecs[order]
     return evals, evecs
 
-def genPIO(fn49, fragmentation=None):
-    # input
-    data = parse_49(fn49)
-    title = data['title']
-    naoao = data['NAOAO']
-    dmnao = data['DNAO']
-    try:
-        fmnao = data['FNAO']
-    except KeyError:
-        pass
-    naolabels = data['NAOlabel']
-    dim = naoao.shape[0]
-    assert dmnao.shape == (dim, dim)
-    
-    # detect fragment orbitals
-    if not fragmentation:
-        prompt = '$ Please input the atom ID of two fragments: (e.g. 1-5,8,13 6-7,9-12)\n$ '
-        command = raw_input(prompt)
-        # command = '1 2'
+def PIO(ffchk, fn49='', fragmentation=None, silent=False):
+    path, fn = os.path.split(ffchk)
+    title, ext = os.path.splitext(fn)
+
+    if fn49 == '':
+        for fn in [os.path.join(path, title + '.49'), 
+                os.path.join(path, title.upper() + '.49'), 
+                os.path.join(path, title.upper() + 'FILE'[len(title)-4:] + '.49')]:
+            if os.path.isfile(fn):
+                fn49 = fn
+                break
+        else:
+            raise UserWarning('.49 file not found.')
+
+    raw = parse_49(fn49)
+    naoao = raw['NAOAO']
+    naolabels = raw['NAOAOlabel']
+    if 'DNAO' in raw:
+        dim_spin = 1
+        dmnao = np.array([raw['DNAO']])
+        if 'FNAO' in raw:
+            fmnao = np.array([raw['FNAO']])
+        else:
+            fmnao is None
     else:
-        command = fragmentation
-    fragments = command.strip().lower().split()
+        assert 'DNAO1' in raw
+        assert 'DNAO2' in raw
+        dim_spin = 2
+        dmnao = np.array([raw['DNAO1'], raw['DNAO2']])
+        if 'FNAO1' in raw and 'FNAO2' in raw:
+            fmnao = np.array([raw['FNAO1'], raw['FNAO2']])
+        else:
+            fmnao is None
+    dim_nao, dim_bs = naoao.shape
+    assert dmnao.shape == (dim_spin, dim_nao, dim_nao)
+    if not silent:
+        print 'Density matrix read in.'
+
+    if fragmentation is None:
+        prompt = '$ Please input the atom ID of two fragments: (e.g. 1-5,8,13 6-7,9-12)\n$ '
+        fragmentation = raw_input(prompt)
+
+    fragments = fragmentation.strip().lower().split()
     assert len(fragments) == 2
     atoms1 = parse_index(fragments[0])
     atoms2 = parse_index(fragments[1])
-    oids1 = np.array([orb for orb in range(dim) if naolabels[orb].center in atoms1])
-    oids2 = np.array([orb for orb in range(dim) if naolabels[orb].center in atoms2])
+    oids1 = np.array([orb for orb in range(dim_nao) if naolabels[orb].center in atoms1])
+    oids2 = np.array([orb for orb in range(dim_nao) if naolabels[orb].center in atoms2])
     assert len(set(oids1)) == len(oids1)
     assert len(set(oids2)) == len(oids2)
     assert not set(oids1).intersection(set(oids2))
-    if set(oids1).union(set(oids2)) != set(range(dim)):
+    if set(oids1).union(set(oids2)) != set(range(dim_nao)):
         print 'Warning: Incomplete fragmentation detected!'
     if set([naolabels[orb].center for orb in oids1]) != set(atoms1):
         print 'Warning: Ghost atoms detected in fragment A!'
     if set([naolabels[orb].center for orb in oids2]) != set(atoms2):
         print 'Warning: Ghost atoms detected in fragment B!'
-    
-    # compute PIOs
-    vals1, vecs1 = eigh(dmnao[oids1,:][:,oids1])
-    vals2, vecs2 = eigh(dmnao[oids2,:][:,oids2])
-    vecs1, vecs2 = vecs1.T, vecs2.T
-    off_block = vecs1.dot(dmnao[oids1,:][:,oids2]).dot(vecs2.T)
-    off_block[abs(off_block) < tol] = 0
-    U, D, V = svd(off_block, full_matrices = True)
-    U = vecs1.T.dot(U)
-    V = V.dot(vecs2)
-    print U.shape, V.shape
-    D2 = D**2
-    print np.array2string(D2, formatter = 
-        {'float_kind': lambda x: ('%.3f' % x if x >= 0.001 else '0')})
-    print 'Total interaction:', D2.sum()
-    
-    pionao = np.eye(dim)
-    pionao[np.array(oids1)[:,None],oids1] = U.T
-    pionao[np.array(oids2)[:,None],oids2] = V
-    if len(oids1) == len(oids2):
-        pass
-    else:
-        if len(oids1) > len(oids2):
-            nullspace = np.array(oids1)[len(oids2):]
-        else:
-            nullspace = np.array(oids2)[len(oids1):]
-        print nullspace
-        dm = pionao.dot(dmnao).dot(pionao.T)
-        evals, evecs = myeigh(dm[nullspace[:,None],nullspace], rev=True)
-        pionao[nullspace] = evecs.dot(pionao[nullspace])
-        try:
-            fm = pionao.dot(fmnao).dot(pionao.T)
-            null2 = nullspace[abs(evals-2)<tol]
-            null0 = nullspace[abs(evals-0)<tol]
-            print null2
-            evals, evecs = myeigh(fm[null2[:,None],null2])
-            print evals
-            pionao[null2] = evecs.dot(pionao[null2])
-            print null0
-            evals, evecs = myeigh(fm[null0[:,None],null0])
-            print evals
-            pionao[null0] = evecs.dot(pionao[null0])
-        except NameError:
-            pass
-   
-    # output
-    d = dict()
-    d['srcfile'] = fn49
-    d['title'] = os.path.splitext(os.path.split(fn49)[1])[0] if title == 'Title Card Required' else title
-    d['dim'] = dim
-    d['fragments'] = (sorted(set([naolabels[orb].center for orb in oids1])), 
+ 
+    data = {}
+    data['srcfile'] = fn49
+    data['title'] = title
+    data['dim'] = (dim_spin, dim_nao, dim_bs)
+    data['fragments'] = (sorted(set([naolabels[orb].center for orb in oids1])), 
         sorted(set([naolabels[orb].center for orb in oids2])))
-    d['oids'] = (oids1, oids2)
-    d['PBI'] = D2
-    d['Contribution'] = D2 / (D2.sum())
-    d['PIOAO'] = pionao.dot(naoao)
-    d['DPIO'] = pionao.dot(dmnao).dot(pionao.T)
-    try:
-        d['FPIO'] = pionao.dot(fmnao).dot(pionao.T)
-    except NameError:
-        pass
+    data['oids'] = (oids1, oids2)
+    data['NAOAO'] = naoao
+    data['DNAO'] = dmnao
+    if fmnao is not None:
+        data['FNAO'] = fmnao
+    data['NAOAOlabel'] = naolabels
+    
+    data.update(genPIO(dmnao, oids1, oids2, fmnao=fmnao, silent=silent))
 
-    dm = d['DPIO']
-    pimonao = np.copy(pionao)
-    for oid1, oid2 in zip(oids1, oids2):
-        D = dm[np.array([oid1, oid2])[:,None],[oid1,oid2]]
-        evals, evecs = myeigh(D,rev=True)
-        # if abs(vals[0] - vals[1]) <= tol:
-            # vecs = eigh(F)[1]
-        pimonao[[oid1,oid2]] = evecs.dot(pionao[[oid1,oid2]])
-    d['PIMOAO'] = pimonao.dot(naoao)
-    d['DPIMO'] = pimonao.dot(dmnao).dot(pimonao.T)
-    try:
-        d['FPIMO'] = pimonao.dot(fmnao).dot(pimonao.T)
-    except NameError:
-        pass
+    npio = data['#PIO']
+    pbi = data['PBI']
+    pbio = np.zeros((dim_spin, dim_nao))
+    for spin in xrange(dim_spin):
+        pbio[spin,oids1[:npio]] = pbi[spin,:npio]
+        pbio[spin,oids2[:npio]] = pbi[spin,:npio]
+    data['PBIO'] = pbio
 
-    return d
+    save(data, ffchk, silent=silent)
+    return data
 
-def saveRawData(data):
-    import pickle
-    with open(os.path.join(os.path.split(data['srcfile'])[0], 
-        data['title']+'_pio.raw'), 'w') as f:
-        pickle.dump(data, f)
+def genPIO(dmnao, oids1, oids2, fmnao=None, tol=1e-6, silent=False):
+    dim_spin = dmnao.shape[0]
+    dim = dmnao.shape[-1]
+    assert dmnao.shape == (dim_spin, dim, dim)
+    oids1, oids2 = np.array(oids1), np.array(oids2)
 
-def saveTxt(data):
+    npio = min(len(oids1), len(oids2))
+    pionao = np.zeros((dim_spin, dim, dim))
+    pbi = np.zeros((dim_spin, npio))
+    pimonao = np.zeros((dim_spin, dim, dim))
+    for spin, dmnao_spin in enumerate(dmnao):
+        vals1, vecs1 = myeigh(dmnao_spin[oids1[:,None],oids1])
+        vals2, vecs2 = myeigh(dmnao_spin[oids2[:,None],oids2])
+        off_block = vecs1.dot(dmnao_spin[oids1[:,None],oids2]).dot(vecs2.T)
+        off_block[abs(off_block) < tol] = 0
+        U, D, V = np.linalg.svd(off_block, full_matrices=True)
+        U = vecs1.T.dot(U)
+        V = V.dot(vecs2)
+        D2 = D**2
+        pionao[spin,oids1[:,None],oids1] = U.T
+        pionao[spin,oids2[:,None],oids2] = V
+        pbi[spin,:npio] = D2 * dim_spin
+
+        dmpio = pionao[spin].dot(dmnao[spin]).dot(pionao[spin].T)
+        nep = int(math.ceil(np.round(dmnao[spin].trace()) * dim_spin / 2))
+        for nullspace in (np.array(oids1)[min(nep,npio):], np.array(oids2)[min(nep,npio):]):
+            evals, evecs = myeigh(dmpio[nullspace[:,None],nullspace], rev=True)
+            pionao[spin,nullspace] = evecs.dot(pionao[spin,nullspace])
+            try:
+                fm = pionao[spin].dot(fmnao[spin]).dot(pionao[spin].T)
+                null2 = nullspace[abs(evals-2)<tol]
+                null0 = nullspace[abs(evals-0)<tol]
+                evals, evecs = myeigh(fm[null2[:,None],null2])
+                pionao[spin,null2] = evecs.dot(pionao[spin,null2])
+                evals, evecs = myeigh(fm[null0[:,None],null0])
+                pionao[spin,null0] = evecs.dot(pionao[spin,null0])
+            except NameError:
+                pass
+
+        for oid1, oid2 in zip(oids1, oids2)[:nep]:
+            D = dmpio[[oid1, oid2]][:,[oid1,oid2]]
+            evals, evecs = myeigh(D,rev=True)
+            assert np.allclose(evals, [2/dim_spin,0], atol=tol)
+            evecs *= np.sign(evecs.sum(axis=1))[:,None]
+            pimonao[spin,[oid1,oid2]] = evecs.dot(pionao[spin,[oid1,oid2]])
+ 
+    if not silent:
+        print 'PIO analysis completed.'
+    data = {'#PIO':npio, 'PIONAO': pionao, 'PIMONAO': pimonao, 'PBI': pbi}
+    return data
+
+def save(data, ffchk, silent=False):
+    savetxt(data, os.path.splitext(ffchk)[0]+'_pio.txt', silent=silent)
+    savefchk(data, ffchk, silent=silent)
+    # saveawc(data, os.path.splitext(ffchk)[0]+'_pio.awc', silent=silent) # Internal use only for now
+
+def savefchk(data, ffchk, silent=False):
+    naoao = data['NAOAO']
+    pionao = data['PIONAO']
+    pimonao = data['PIMONAO']
+    pbio = data['PBIO']
+
+    pioao = pionao.dot(naoao)
+    pimoao = pimonao.dot(naoao)
+
+    quicksave(ffchk, pioao, pbio, suffix='_pio', overwrite=True)
+    quicksave(ffchk, pimoao, pbio, suffix='_pimo', overwrite=True)
+    if not silent:
+        print 'PIO/PIMO saved to fchk files.'
+
+def savetxt(data, ofn, silent=False):
     atoms1, atoms2 = data['fragments']
+    dim_spin, dim_pio, dim_bs = data['dim']
     oids1, oids2 = data['oids']
-    ixn = data['PBI']
-    dm = data['DPIO']
-    fm = data.get('FPIO', np.zeros_like(dm))
-    
-    pops1 = np.diag(dm)[oids1]
-    pops2 = np.diag(dm)[oids2]
-    energies1 = np.diag(fm)[oids1]
-    energies2 = np.diag(fm)[oids2]
-    total = sum(ixn)
-    contrib = ixn / total * 100
-    cum = np.cumsum(contrib)
-    if not fm is None:
-        ie = [fm[i][j] for i, j in zip(oids1, oids2)]
-    else:
-        ie = [0 for i, j in zip(oids1, oids2)]
-    
-    popb = []
-    popa = []
-    eb = []
-    ea = []
-    rie = []
-    for d11, d22, f11, f22, d12d12, f12 in \
-        zip(pops1, pops2, energies1, energies2, ixn, ie):
-        d12 = sqrt(d12d12)
-        D = np.array([[d11, d12], [d12, d22]])
-        F = np.array([[f11, f12], [f12, f22]])
-        vals, vecs = eigh(D)
-        if abs(vals[0] - vals[1]) <= tol:
-            vecs = eigh(F)[1]
-        newD = vecs.dot(D).dot(vecs.T)
-        newF = vecs.dot(F).dot(vecs.T)
-        popb.append(newD[1, 1])
-        popa.append(newD[0, 0])
-        eb.append(newF[1, 1])
-        ea.append(newF[0, 0])
-        rie.append(newF[0, 1])
-    
-    text = zip(np.array(oids1) + 1, pops1, energies1, 
-        np.array(oids2) + 1, pops2, energies2, 
-        ixn, contrib, cum, ie, 
-        eb, ea, popb, popa, rie)
-    # for line in text:
-        # print line
-    
-    mod = '%4d%10f%12f%4d%10f%12f%10f%10f%8.2f%12f%12f%12f%10f%10f%12f'
+    naoao = data['NAOAO']
+    pbi = data['PBI']
+    pionao = data['PIONAO']
+    pimonao = data['PIMONAO']
+    dmnao = data['DNAO']
+    fmnao = data.get('FNAO', np.zeros_like(dmnao))
+    npio = data['#PIO']
+
+    dmpio = np.array([pionao[_].dot(dmnao[_]).dot(pionao[_].T) for _ in xrange(dim_spin)])
+    fmpio = np.array([pionao[_].dot(fmnao[_]).dot(pionao[_].T) for _ in xrange(dim_spin)])
+    dmpimo = np.array([pimonao[_].dot(dmnao[_]).dot(pimonao[_].T) for _ in xrange(dim_spin)])
+    fmpimo = np.array([pimonao[_].dot(fmnao[_]).dot(pimonao[_].T) for _ in xrange(dim_spin)])
+    total = pbi.sum()
+
+    mod = '%5d%1s %10.5f %10.5f %5d%1s %10.5f %10.5f %10.5f %10.5f %8.2f %10.5f %10.5f %10.5f %10.5f %10.5f %10.5f'
     modt = re.sub('(\.\d+)?[fd]', 's', mod)
-        
-    with open(os.path.join(os.path.split(data['srcfile'])[0], 
-        data['title']+'_pio.txt'), 'w') as f:
+    with open(ofn, 'w') as f:
         f.write('PIO %s\n' % VERSION)
         f.write('%s\n' % VERSIONTEXT)
         f.write('Fragment A: %s (Orbitals: %s)\n' % (rev_parse_index(atoms1), rev_parse_index(oids1+1)))
         f.write('Fragment B: %s (Orbitals: %s)\n' % (rev_parse_index(atoms2), rev_parse_index(oids2+1)))
-        f.write('Fragment A'.center(26))
-        f.write('Fragment B'.center(26))
+        if dim_spin == 1:
+            f.write('Total interaction: %f\n' % total)
+        else:
+            f.write('Total interaction: %f\n' % total)
+            f.write('Alpha interaction: %f\n' % pbi[0].sum())
+            f.write('Beta  interaction: %f\n' % pbi[1].sum())
         f.write('\n')
-        f.write(modt % ('Orb', 'Pop   ', 'E    ', 'Orb', 'Pop   ', 'E    ', 
-            'Ixn   ', 'Contrib%', 'Cum%', 'IE   ', 
-            'EB   ', 'EA   ', 'PopB  ', 'PopA  ', 'RIE   '))
-        f.write('\n')
-        
-        while text:
-            f.write(mod % text.pop(0))
+
+        for spin in xrange(dim_spin):
+            nep = int(math.ceil(np.round(dmnao[spin].trace()) * dim_spin / 2))
+            pbispin = pbi[spin]
+            pop1 = np.diag(dmpio[spin])[oids1]
+            pop2 = np.diag(dmpio[spin])[oids2]
+            energy1 = np.diag(fmpio[spin])[oids1]
+            energy2 = np.diag(fmpio[spin])[oids2]
+            popb = np.diag(dmpimo[spin])[oids1]
+            popa = np.diag(dmpimo[spin])[oids2]
+            energyb = np.diag(fmpimo[spin])[oids1]
+            energya = np.diag(fmpimo[spin])[oids2]
+            contrib = pbispin / total * 100
+            cum = np.cumsum(contrib)
+            ie = [fmpio[spin][i][j] for i, j in zip(oids1, oids2)]
+            rie = [fmpimo[spin][i][j] for i, j in zip(oids1, oids2)]
+
+            if dim_spin == 2:
+                spincode = 'ALPHA' if spin == 0 else 'BETA'
+                f.write('---%s---\n' % spincode)
+            f.write('Fragment A'.center(26))
+            f.write('Fragment B'.center(26))
             f.write('\n')
-        
-        print 'PIO result saved to file.\n'
+            f.write(modt % ('Or', 'b', 'Pop   ', 'E    ', 'Or', 'b', 'Pop   ', 'E    ', 
+                'Ixn   ', 'Contrib%', 'Cum%', 'IE   ', 
+                'EB   ', 'EA   ', 'PopB  ', 'PopA  ', 'RIE   '))
+            f.write('\n')
 
-def saveFChk(data, mfn):
-    pioao = data['PIOAO']
-    quicksave(mfn, pioao, data.get('FPIO', np.zeros(pioao.shape[0])), 
-        suffix='_pio', overwrite=True)
-    pimoao = data['PIMOAO']
-    quicksave(mfn, pimoao, data.get('FPIMO', np.zeros(pimoao.shape[0])), 
-        suffix='_pimo', overwrite=True)
+            if dim_spin == 2:
+                orbspin = 'a' if spin == 0 else 'b'
+            else:
+                orbspin = ''
+            text = zip(oids1 + 1, repeat(orbspin), pop1, energy1, 
+                    oids2 + 1, repeat(orbspin), pop2, energy2, 
+                    pbispin[:min(nep,npio)], contrib, cum, ie, 
+                    energyb, energya, popb, popa, rie)
+            while text:
+                f.write(mod % text.pop(0))
+                f.write('\n')
+            f.write('\n\n')
 
-def saveAll(data, mfn):
-    saveRawData(data)
-    saveTxt(data)
-    saveFChk(data, mfn)
+        f.close()
+    if not silent:
+        print 'PIO result saved to file.'
+
+def saveawc(data, ofn, silent=False):
+    dim_spin, dim_pio, dim_bas = data['dim']
+    naolabels = data['NAOAOlabel']
+    pionao = data['PIONAO']
+    pbio = data['PBIO']
+
+    assert pbio.shape == (dim_spin, dim_pio)
+    assert len(naolabels) == dim_pio
+    assert pionao.shape == (dim_spin, dim_pio, dim_pio)
+
+    atoms = sorted(set([nao.center for nao in naolabels]))
+    awc = np.zeros((dim_spin, dim_pio, len(atoms)))
+    for spin in xrange(dim_spin):
+        for pio in xrange(dim_pio):
+            for nao in xrange(dim_pio):
+                awc[spin, pio, naolabels[nao].center-1] += pionao[spin,pio,nao] ** 2
+
+    with open(ofn, 'w') as f:
+        for spin in xrange(dim_spin):
+            if dim_spin == 2:
+                spincode = 'ALPHA' if spin == 0 else 'BETA'
+                f.write('---%s---\n' % spincode)
+            for pio in xrange(dim_pio):
+                if pbio[spin,pio] >= 0.1 / dim_spin:
+                    f.write('PIO %-4d\n' % (pio + 1))
+                    for at in xrange(len(atoms)):
+                        if awc[spin, pio, at] > 0.1:
+                            f.write('        %-4d         %8.4f\n' % (at + 1, awc[spin, pio, at]))
+                        for nao in xrange(dim_pio):
+                            if naolabels[nao].center == at + 1 and pionao[spin,pio,nao] ** 2 >= 0.1:
+                                f.write('            %-8s %8.4f\n' % (naolabels[nao].label(), pionao[spin,pio,nao] ** 2))
+            f.write('\n\n')
+        f.close()
+    if not silent:
+        print 'Atom/NAO contributions saved to file.'
 
 def main():
-    fragmentation = ' '.join(sys.argv[2:4]) or None
     ffchk = sys.argv[1]
-    f49 = os.path.splitext(ffchk)[0].upper() + '.49'
-    data = genPIO(f49, fragmentation)
-    saveAll(data, ffchk)
+    fn49 = sys.argv[2] if sys.argv[2:] else ''
+    fragmentation = sys.argv[3:5] if sys.argv[3:] else None
+    PIO(ffchk, fn49, fragmentation)
 
 if __name__ == '__main__':
     main()
